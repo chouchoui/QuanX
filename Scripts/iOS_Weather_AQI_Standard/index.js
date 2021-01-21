7bcbdcd3caf86a36176e80835e0d3e8f1a8521f3
chouchoui <vei@live.cn>
2021-01-21 09:12:12 +0800

    修正天气脚本在 AQI 爆表（超过 500）时显示 “未知” 的问题

--- Scripts/iOS_Weather_AQI_Standard/index.js
+++ Scripts/iOS_Weather_AQI_Standard/index.js
@@ -42,7 +42,7 @@ function classifyAirQualityLevel(aqiIndex) {
     return AirQualityLevel.UNHEALTHY;
   } else if (aqiIndex >= 201 && aqiIndex <= 300) {
     return AirQualityLevel.VERY_UNHEALTHY;
-  } else if (aqiIndex >= 301 && aqiIndex <= 500) {
+  } else if (aqiIndex >= 301) {
     return AirQualityLevel.HAZARDOUS;
   }
 }
