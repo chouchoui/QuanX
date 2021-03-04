d450112260a6d53d4741cd38e2e54b1366ef8675
chouchoui <vei@live.cn>
2021-03-04 13:48:17 +0800

    update ithome ad

--- Scripts/ithome/ithome.ad.js
+++ Scripts/ithome/ithome.ad.js
@@ -1,12 +1,15 @@
 const mobileWebRegex = /^https:\/\/m\.ithome\.com\/api\/news\/newslistpageget/;
 const appRegex = /^https:\/\/api\.ithome\.com\/json\/(listpage|newslist)\/news/;
+const appSlideRegex = /^https:\/\/api\.ithome\.com\/json\/slide\/index/;
+
+const body = JSON.parse($response.body);
 
 if (mobileWebRegex.test($request.url)) {
-  const body = JSON.parse($response.body);
   body.Result = body.Result.filter((r) => r.NewsTips.every((t) => t.TipName !== "广告"));
-  $done(JSON.stringify(body));
 } else if (appRegex.test($request.url)) {
-  const body = JSON.parse($response.body);
   body.newslist = body.newslist.filter((n) => !n.aid);
-  $done(JSON.stringify(body));
+} else if (appSlideRegex.test($request.url)) {
+  body = body.filter((i) => !i.isad);
 }
+
+$done(JSON.stringify(body));
