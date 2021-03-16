98c354b87cd25b362c56a9d1658db3ce5f02f13d
chouchoui <vei@live.cn>
2021-03-16 23:00:12 +0800

    update jike live

--- Scripts/jike/jike.live.js
+++ Scripts/jike/jike.live.js
@@ -1,7 +1,7 @@
 let body = JSON.parse($response.body);
 
 if (/^https:\/\/api\.ruguoapp\.com\/1\.0\/recommendFeed\/list/.test($request.url)) {
-  body.data = body.data.filter((d) => !d.live);
+  body.data = body.data.filter((d) => !d.live && !d.lives);
   body = JSON.stringify(body);
 }
 
