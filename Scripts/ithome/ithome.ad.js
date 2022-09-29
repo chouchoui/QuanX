48af795700a897ba143b264bba82b8a4f8357e2e
chouchoui <14866249+chouchoui@users.noreply.github.com>
2022-09-29 11:14:02 +0800

    fix(ithome):  fix ad

--- Scripts/ithome/ithome.ad.js
+++ Scripts/ithome/ithome.ad.js
@@ -1,6 +1,7 @@
 const mobileWebRegex = /^https:\/\/m\.ithome\.com\/api\/news\/newslistpageget/;
 const appRegex = /^https:\/\/api\.ithome\.com\/json\/(listpage|newslist)\/news/;
 const appSlideRegex = /^https:\/\/api\.ithome\.com\/json\/slide\/index/;
+const newAppFeed = /^https:\/\/napi\.ithome\.com\/api\/(news|topmenu)\/(getfeeds|index)/;
 
 let body = JSON.parse($response.body);
 
@@ -12,6 +13,20 @@ if (mobileWebRegex.test($request.url)) {
   const newList = body.filter((i) => !i.isad);
   body.splice(0, body.length);
   body.push(...newList);
+} else if (newAppFeed.test($request.url)) {
+  let list = body.data.list;
+  const newList = [];
+  for (const item of list) {
+    if (item.feedContent.smallTags && item.feedContent.smallTags.some((s) => s.text === "广告")) {
+      continue;
+    }
+    if (item.feedContent.focusNewsData) {
+      const newNewsData = item.feedContent.focusNewsData.filter((n) => !n.isAd);
+      item.feedContent.focusNewsData = newNewsData;
+    }
+    newList.push(item);
+  }
+  body.data.list = newList;
 }
 
 body = JSON.stringify(body);
